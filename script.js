f9c58e27171a7d6f8ee2854d18d8c15593e52f9c
Alexsander Gianasi <206681800+AlexsanderGianasi@users.noreply.github.com>
2026-03-29 12:11:01 -0300

    ,,,

---
+++ script.js
@@ -0,0 +1,8 @@
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
